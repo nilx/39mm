7ffd80e6ce61b44f49dd043bb5963b6cdd7773fa
Nicolas Limare <nicolas@limare.net>
2017-09-05 14:13:22 +0000

    Use files

--- chef/cookbooks/39mm/recipes/base.rb
+++ chef/cookbooks/39mm/recipes/base.rb
@@ -4,3 +4,6 @@ package 'emacs-nox'
 package 'apt-utils'
 package 'aptitude'
 package 'bash-completion'
+
+package 'w3m'
+package 'host'
--- chef/cookbooks/39mm/recipes/default.rb
+++ chef/cookbooks/39mm/recipes/default.rb
@@ -1,5 +1,5 @@
 include_recipe "#{cookbook_name}::apt"
 include_recipe "#{cookbook_name}::base"
-#include_recipe "#{cookbook_name}::www"
+include_recipe "#{cookbook_name}::www"
 #include_recipe "#{cookbook_name}::smtp"
 #include_recipe "#{cookbook_name}::imap"
